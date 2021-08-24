5d37c1bc5232aeaca9aeb0879db219cd78a11147
derek <derekle.creative@gmail.com>
2021-08-24 01:31:27 -0500

    commit tree bab8cc17a1682d7aa07ae1afe98ba782f05ea17a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+class Cat {
+    constructor(name, sex){
+        console.log(`${name} says meow!`)
+        this.speak = function(){return `${name} says meow!`}
+    }
+}
+  
+class Dog {
+    constructor(name, sex){
+        this.speak = function(){return `${name} says woof!`}
+    }
+}
+  
+class Bird {
+    constructor(name, sex){
+        if (sex == 'male'){
+        this.speak = function(){return `It's me! ${name}, the parrot!`}
+        } else {
+            this.speak = function(){return `${name} says squawk!`}
+        }
+    }
+} 
